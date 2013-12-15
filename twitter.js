f041b608777add754b3f96252d45304cc353807b
lealbaugh <l.e.albaugh@gmail.com>
2013-12-14 22:58:54 -0500

    Adds stubs for game and responses, starts to modify DB abilities.

--- twitter.js
+++ twitter.js
@@ -1,3 +1,6 @@
+var game = require("./game.json");
+var responses = require("./responses");
+
 //--API keys setup for Twitter and MongoHQ----------------
 
 var keys = require("./apikeys.js");
@@ -7,7 +10,7 @@ var mongoUri = keys.mongoURL;
 
 var util = require("util");
 var twitter = require('ntwitter'); //https://github.com/AvianFlu/ntwitter
-var my_username = keys.id_str;
+var my_user_id = keys.id_str;
 var game_root_status_id_str = keys.game_root_status_id_str;
 
 var tweeter = new twitter({
@@ -18,97 +21,115 @@ var tweeter = new twitter({
 });
 
 
-function checkIntoDatabase(thistweet, collectionname) {
-	mongo.Db.connect(mongoUri, function (err, db) {
+function getState(user) {
+	mongo.Db.connect(mongoUri, function (err, db, user) {
 		if (err) {
 			console.log(err);
 		}
-		// "The findAndModify command atomically modifies and returns a single document. By default, the returned document does not include the modifications made on the update. To return the document with the modifications made on the update, use the "new" option." http://docs.mongodb.org/manual/reference/command/findAndModify/
-		db.collection(collectionname).findAndModify({"tweet_id": thistweet.tweet_id}, [], {
-			$set: {
-				"tweet_id": thistweet.tweet_id,
-				"author": thistweet.author,
-				"author_id": thistweet.author_id,
-				"text": thistweet.text,
-				"retweet_count": thistweet.retweet_count,
-				"favorite_count": thistweet.favorite_count,
-				}
-			}, {"upsert": "true", "new": "true"}, function(err, object, thistweet, collectionname) {
-				if (err) {
-					console.warn(err.message);
-				}
-				else {
-					console.log("updated collection");
-					if (object.retweeted_by_bot != "true") {
-						if (tweetQualifiesForRetweet(object)) {
-							retweet(object.tweet_id, collectionname);
-						}
-					}
+
+		db.collection("users").find({"user":user}).toArray(function (err, docs, user) {
+			var currentstate;
+			if (docs.length==0) {
+				//add user as new to db, with state="start"
+				currentstate = "start";
+				newuser = {"user":user, "state":[currentstate]};
+
 			}
+			else if (docs.length==1) {
+				var statehistory = docs[0].state;
+				currentstate = statehistory[statehistory.length-1];
+			}
+			else {
+				console.log("Username conflict?");
+			}
+
+
+
+
+			db.close();
 		});
 	});
 }
 
 
-function retweet(tweet_id, collectionname) {
-	retweeter.retweetStatus(tweet_id, function(data){
-		console.log("retweeted!");
-		});
 
+function setState(user, state) {
 	mongo.Db.connect(mongoUri, function (err, db, collectionname) {
 		if (err) {
 			console.log(err);
 		}
 		else {
-			db.collection("tweets").findAndModify({"tweet_id": tweet_id}, [], {$set: { "retweeted_by_bot": "true" } }, {}, function(err, object) {
+			db.collection("users").findAndModify({"user": user}, [], {$set: { "state": state } }, {}, function(err, object) {
 				if (err) console.warn(err.message);
-				else console.log("logged retweet!");
+				else console.log("Changed user "+user+" to state "+state+".");
 			});
 		}
 	});
 }
 
 
-function condenseTweet(tweet) {
-	return {
-		"tweet_id": tweet.id_str,
-		"author": tweet.user.id_str,
-		"author_id": tweet.user.name,
-		"text": tweet.text,
-		"retweet_count": tweet.retweet_count,
-		"favorite_count": tweet.favorite_count,
-		"retweeted_by_bot": "false",
-		}
+function tweet(user, response, in_response_to) {
+	response = "@"+user+" "+response;
+	tweeter.updateStatus(text, {"in_reply_to_status_id_str":in_response_to}, function() {
+		console.log("I said: "+response);
+	})
 }
 
-function gameplay(user, move) {
-	console.log(user+" said "+move);
+function updateState(user, newstate, in_response_to) {
+	setState(user, newstate);
+	
+	//tweet new status
+	response = responses[newstate];
+	//remember to include response for ""
+	tweet(user, response, in_response_to);
+}
+
+
+// A couple of ways to do this:
+// - check which tweet a user is responding to:
+		// if(data.in_reply_to_status_id_str == game_root_status_id_str) {
+		// 	newPlayer();
+		// }
+// 		- this allows them to branch the story (maybe only until they reach an ending?)
+// - store user state completely in database, don't worry about what they're responding to
+// Hard to tell which one is more intuitive?
+
+
+
+function gameplay(user, text, message_id) {
+	console.log(user+" said: "+text);
 
 	//sanitize move input: remove names, etc
+	cleanText(text);
 
 	//get user's current state from database
+	var currentstate = getState(user);
+
 	//match input against possible moves from user's current state
+	var nextstate = "";
+
+	for (phrase in game[currentstate]) {
+		if (text.search(phrase)>=0) {
+			nextstate = game[currentstate][phrase];
+		}
+	}
 	//update user's state
-	//tweet new status
+	updateState(user, nextstate, message_id);
 }
 
-// if(data.in_reply_to_status_id_str == game_root_status_id_str) {
-// 	newPlayer();
-// }
+
 
 function openUserStream(tweeter){
 	tweeter.stream('user', {}, function(stream){
 		console.log("Making my stream.");
 		stream.on('data', function (data){
-			if (data.user && data.user.id_str != my_username) {
+			if (data.user && data.user.id_str != my_user_id) {
 				user = data.user.id_str;
-				move = data.text;
-				gameplay(user, move);
+				message_id = data.id_str;
+				text = data.text;
+				gameplay(user, text, message_id);
 			}
-			else { //actually this is messy, because it does this for all other events too
-				user = data.in_reply_to_screen_name;
-				response = data.text;
-				console.log("I told "+user+": "+response);
+			else { //actually this is messy, because it does this for all other events too	
 			}
 			console.log("--------------------------------------");
 		});
@@ -122,5 +143,7 @@ console.log("Starting up...");
 // 	console.log(err);
 //     console.log(data);
 // });
+getState("lea");
+getState("Timmy");
 
-openUserStream(tweeter);
+// openUserStream(tweeter);
